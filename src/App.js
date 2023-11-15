27195a77eff00e438d342c2e1657e33bcecbb61d
shynu S.J <shynujose11@gmail.com>
2023-11-16 00:10:53 +0530

    resolved eventlistener accumulation by cleanup functon

--- src/App.js
+++ src/App.js
@@ -296,11 +296,16 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   }
   useEffect(
     function () {
-      document.addEventListener("keydown", function (e) {
+      function callback(e) {
         if (e.code === "Escape") {
           onCloseMovie();
         }
-      });
+      }
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
     },
     [onCloseMovie]
   );
