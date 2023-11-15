1e5c2d06c8cac85da2ab2092ad28bd1e56f7c4ed
shynu S.J <shynujose11@gmail.com>
2023-11-15 18:20:58 +0530

    Adding a New Effect: Changing Page Title

--- src/App.js
+++ src/App.js
@@ -304,6 +304,11 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     },
     [selectedId]
   );
+
+  useEffect(() => {
+    if (!title) return;
+    document.title = `Movie | ${title}`;
+  }, [title]);
   return (
     <div className="details">
       {isLoading ? (
